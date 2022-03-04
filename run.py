38a71ee2fb82b32890614e7f52a49d518d8d098b
siddhuA98 <siddhu.arani@gmail.com>
2022-03-04 17:21:28 +0530

    final commit

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import create_app, db  # app/__init__ (ROOT PACKAGE)
 from app.auth.models import User
-from sqlalchemy import exc
+
 
 flask_app = create_app('prod')
 with flask_app.app_context():
