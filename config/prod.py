1a9373e3f21e6b5f7f16cedb4603d43dc48eed75
siddhuA98 <siddhu.arani@gmail.com>
2022-03-07 12:41:26 +0530

    final

--- config/prod.py
+++ config/prod.py
@@ -2,5 +2,5 @@ import os
 
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://bqdvzyeemsbdlu:04ff2f5825cdaf39921c053a256d616729ffc3dd8394a708d8227f4f7bbf10f3@ec2-52-70-186-184.compute-1.amazonaws.com:5432/d9smih0m472knp'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
