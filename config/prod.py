3dc8e18ed4d1abb1a58601f730be909bf648d6b2
siddhuA98 <siddhu.arani@gmail.com>
2022-03-07 12:55:15 +0530

    final

--- config/prod.py
+++ config/prod.py
@@ -2,5 +2,5 @@ import os
 
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "").replace("postgres://", "postgresql://")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
